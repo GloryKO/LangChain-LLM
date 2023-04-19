5447ba7e2ba883b40d4b4958cb55f999adf896d8
GloryKO <koladeg2022@gmail.com>
2023-04-19 10:03:02 -0700

    added API KEY

--- lang.py
+++ lang.py
@@ -3,9 +3,10 @@ import os
 #import langchain
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 #calling llms directly  on user input 
-llm = openAI(temperature =0.9)
+llm = OpenAI(temperature =0.9)
 text = "What would be a good company name for a company that makes colorful socks?"
 print(llm(text))
 
@@ -16,7 +17,16 @@ prompt = PromptTemplate(
 )
 print(prompt.format(product="colorful socks"))
 
+
 #chains : combine LLMs and PromptTemplate in multi-steps workflow
+llm = OpenAI(temperature = 0.9)
 
+prompt = PromptTemplate(
+    input_variables = ["product"],
+    template = "What is a good name for a company that makes {product}?"
+)
 
+chain =LLMChain(llm,prompt=prompt)
 
+#run the chain
+chain.run("colorful socks")
