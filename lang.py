1d6062ea5636b02ef47599da4ce5ff53790df717
GloryKO <koladeg2022@gmail.com>
2023-04-25 10:02:47 -0700

    added one line comment

--- lang.py
+++ lang.py
@@ -8,21 +8,12 @@ from langchain.chains import LLMChain
 
 # 1 calling llms directly  on user input 
 
-# llm = OpenAI(temperature =0.9)
-# text = "What would be a good company name for a company that makes colorful socks?"
-# print(llm(text))
-
 def generate_text(text):
     llm = OpenAI(temperature =0.9)
     return llm(text)
 
 #2 taking user input and contsucting a prompt from it and then formatting the prompt . we use PromptTemplate
 
-# prompt = PromptTemplate(
-#     input_variables=["product"],
-#     template="What is a good name for a company that makes {product}?",
-# )
-# print(prompt.format(product="colorful socks"))
 
 def generate_prompt(product):
     prompt = PromptTemplate(input_variables=["product"], template="What is a good name for a company that makes {product}?")
@@ -30,17 +21,6 @@ def generate_prompt(product):
 
 
 #3 chains : combine LLMs and PromptTemplate in multi-steps workflow
-# llm = OpenAI(temperature = 0.9)
-
-# prompt = PromptTemplate(
-#     input_variables = ["product"],
-#     template = "What is a good name for a company that makes {product}?"
-# )
-
-# chain =LLMChain(llm,prompt=prompt)
-
-# #run the chain
-# chain.run("colorful socks")
 
 def generate_chain(product):
     llm =OpenAI(temperature=0.9)
